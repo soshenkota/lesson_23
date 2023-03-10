b52e5d3cf1e95cad011a14d46df064a2c5e44a40
soshenkota <114571243+soshenkota@users.noreply.github.com>
2023-03-10 12:27:29 +0200

    123

---
+++ lesson_23/js/script.js
@@ -0,0 +1,29 @@
+"use strict"
+// console.log('35' + -"25");
+// console.log('35' * "22");
+// console.log('558' > 22++);
+// let usersCounter = 0;
+// let newUsers = usersCounter++;
+// console.log(newUsers);
+// console.log(!false && 11 || 18 && !"");
+// let name = 0;
+// console.log(name ?? "Без імені");
+
+// for (let name = 1; name < 6; ++name) {
+//    console.log(name);
+// }
+
+// let num = 8;
+// while (num) {
+//    console.log(num);
+//    num--;
+// }
+// let fls = "фрилансер";
+// let text = `Привіт! Я ${fls}`;
+// console.log(text);
+// let text = `фрилансер`;
+// console.log(text[5]);
+// let text = `фрилансер`;
+// console.log(text.toUpperCase());
+let text = `фрилансер`;
+console.log(text.includes('лан', 4));
